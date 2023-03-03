8b4857dcf2845286130ef8987554d604983ff27c
AbhiGhare <ghareabhishek4@gmail.com>
2023-03-03 11:14:15 +0530

    new project

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("mobile-nav-toggle").style.display="none"
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("mobile-nav-toggle").style.display="block"
+
+  }
